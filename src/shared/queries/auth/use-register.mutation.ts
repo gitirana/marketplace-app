220fb5d5e2085da1429eedaa6ad8fe0f4cc79fc4
Tauã Manoel Gitirana <thaygitirana@gmail.com>
2026-02-20 22:05:05 -0300

    feat: create use-register mutation fn

---
+++ src/shared/queries/auth/use-register.mutation.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query"
+import * as authService from "../../services/auths.service"
+import type { RegisterHttpParams } from "../../interfaces/http/register"
+
+export const useRegisterMutation = () => {
+  const mutation = useMutation({
+    mutationFn: (userData: RegisterHttpParams) => authService.register(userData),
+    onSuccess: (response) => {
+      console.log(response)
+    },
+    onError: (error) => {
+      console.log(error)
+    }
+  })
+
+  return mutation
+}
