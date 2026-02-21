d9cf37e70692ddd5591649cea76ad4e7e548cfb4
Tauã Manoel Gitirana <thaygitirana@gmail.com>
2026-02-20 21:19:55 -0300

    feat: add private routes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout() {
       }}>
         <Stack.Screen name="login" />
         <Stack.Screen name="register" />
+        <Stack.Screen name="(private)" />
       </Stack>
     </QueryClientProvider>
   )
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,15 @@
 import { Redirect } from "expo-router";
 
 export default function App() {
+  const userData = {
+    token: "aksfjaks",
+    name: "User teste"
+  }
+
+  if (userData) {
+    return <Redirect href={"/(private)/home"} />
+  }
+
   return (
     <Redirect href={"/login"} />
   )
