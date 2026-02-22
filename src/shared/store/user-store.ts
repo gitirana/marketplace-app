71221e22c7186eb847b955d1ed24a5c1c9c3bd5f
Tauã Manoel Gitirana <thaygitirana@gmail.com>
2026-02-22 09:20:35 -0300

    feat: add async storage persist

--- src/shared/store/user-store.ts
+++ src/shared/store/user-store.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand"
 import type { UserInterface } from "../interfaces/user"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { persist, createJSONStorage } from "zustand/middleware"
 
 interface SetSessionParams {
   user: UserInterface
@@ -22,16 +24,24 @@ export interface UserStore {
   updateTokens: (updateTokensData: UpdateTokensParams) => void;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
-  user: null,
-  token: null,
-  refreshToken: null,
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      refreshToken: null,
 
-  logout: () => { },
-  setSession: (sessionData) => {
-    set({ ...sessionData })
-  },
-  updateTokens: (updateTokensData) => {
-    set({ ...updateTokensData })
-  }
-}))
+      logout: () => set({ user: null, token: null, refreshToken: null }),
+      setSession: (sessionData) => {
+        set({ ...sessionData })
+      },
+      updateTokens: (updateTokensParams) => {
+        set({ ...updateTokensParams })
+      },
+    }),
+    {
+      name: 'marketplace-auth',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+)
